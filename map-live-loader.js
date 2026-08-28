cd43fd40e536717f38f71b6d951b79c02c39c888
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 08:50:06 +0100

    Refresh title chain for exact guest melee key

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=13";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=14";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
