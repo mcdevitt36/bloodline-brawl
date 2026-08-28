dc19b83b2bab59aca14abe4948029e8b549177c1
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 22:59:40 +0100

    Load high quality Warfare specials and ultras

--- warfare-v5-loader.js
+++ warfare-v5-loader.js
@@ -2,4 +2,5 @@ import './warfare-weapon-polish-v5.js?v=1';
 import './warfare-map-polish-v5.js?v=1';
 import './warfare-interactive-v7.js?v=1';
 import './warfare-character-polish-v8.js?v=1';
+import './warfare-specials-v9.js?v=1';
 import './warfare-v4.js?v=2';
