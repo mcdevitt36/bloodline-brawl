65f4330782dcf2be77eadc63798ffd8fc41b6348
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 22:16:45 +0100

    Switch Warfare interactions from v6 to stabilized v7

--- warfare-v5-loader.js
+++ warfare-v5-loader.js
@@ -1,4 +1,4 @@
 import './warfare-weapon-polish-v5.js?v=1';
 import './warfare-map-polish-v5.js?v=1';
-import './warfare-interactive-v6.js?v=1';
+import './warfare-interactive-v7.js?v=1';
 import './warfare-v4.js?v=2';
