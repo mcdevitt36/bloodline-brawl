e05e81a931fc912ae38092cbfcd5b05da0638147
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 22:42:44 +0100

    Load Warfare character polish v8

--- warfare-v5-loader.js
+++ warfare-v5-loader.js
@@ -1,4 +1,5 @@
 import './warfare-weapon-polish-v5.js?v=1';
 import './warfare-map-polish-v5.js?v=1';
 import './warfare-interactive-v7.js?v=1';
+import './warfare-character-polish-v8.js?v=1';
 import './warfare-v4.js?v=2';
