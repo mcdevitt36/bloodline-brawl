6086bf2efcb3da594c0d4f86d6d3b5b91185a2d5
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 04:38:50 +0100

    Reload louder menu audio polish

--- map-live-loader.js
+++ map-live-loader.js
@@ -11,7 +11,7 @@
     "load",
     () => {
       const audioPolish = document.createElement("script");
-      audioPolish.src = "audio-polish-v5.js?v=4";
+      audioPolish.src = "audio-polish-v5.js?v=5";
       document.body.appendChild(audioPolish);
     },
     { once: true }
