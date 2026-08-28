5b4e3b83756442ee8a12ae1675c70b0e1296c657
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 06:02:17 +0100

    Load single player alternate controls

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=4";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=5";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
