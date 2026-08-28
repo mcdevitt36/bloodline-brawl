fbeb3ba74aa63e2c3ed6d3f8da78d8154b31b5f5
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 06:29:06 +0100

    Refresh post-match results loader

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=6";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=7";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
