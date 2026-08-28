fb0c2c5d9bfce2e47b4851c44c8b0d6af8e36afa
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 23:24:27 +0100

    Load alternate Warfare fire and aim controls

--- warfare-v5-loader.js
+++ warfare-v5-loader.js
@@ -4,4 +4,5 @@ import './warfare-interactive-v7.js?v=1';
 import './warfare-character-polish-v8.js?v=1';
 import './warfare-specials-v9.js?v=1';
 import './warfare-hub-polish-v10.js?v=1';
+import './warfare-keyboard-fire-v11.js?v=1';
 import './warfare-v4.js?v=2';
