49953447bf67ff62a901730db8df84ef80697727
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 07:25:32 +0100

    Refresh online performance loader

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=11";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=12";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
