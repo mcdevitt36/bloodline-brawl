b970da0167c1e398a949b7c63b2511452251f5bf
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 05:41:39 +0100

    Refresh Connor beard v5 loader

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=3";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=4";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
