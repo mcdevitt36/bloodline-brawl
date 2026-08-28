ccffc9e910e1cee162f320652b304eeff12e79a8
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 05:29:56 +0100

    Load Connor beard polish v2

--- title-position-fix-v9.js
+++ title-position-fix-v9.js
@@ -65,7 +65,7 @@
     "load",
     () => {
       const connorStubblePolish = document.createElement("script");
-      connorStubblePolish.src = "connor-stubble-polish-v1.js?v=1";
+      connorStubblePolish.src = "connor-stubble-polish-v1.js?v=2";
       document.body.appendChild(connorStubblePolish);
     },
     { once: true }
