ddd7042c765a20ad626d9ca562077d88b3b34f5c
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 08:42:12 +0100

    Refresh title loader for online guest fix

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=12";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=13";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
