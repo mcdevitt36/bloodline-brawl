e6ccf6bb87f0f899a233a6b1d0858c0aa9ecc692
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 05:30:22 +0100

    Refresh Connor beard polish loader

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=2";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=3";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
