96a4e3f03fdc5bfafd058c5e21e272e5e53640ce
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-28 17:20:13 +0100

    Refresh family challenges loader

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=14";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=15";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
