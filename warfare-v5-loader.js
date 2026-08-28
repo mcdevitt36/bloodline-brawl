dd98cae778da9df2e75e0afd4b2a1c1ae83a52d0
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-29 00:04:43 +0100

    Load Warfare gameplay audio v20

--- warfare-v5-loader.js
+++ warfare-v5-loader.js
@@ -13,5 +13,6 @@ import './warfare-stability-v17.js?v=1';
 import './warfare-vertical-combat-v16.js?v=1';
 import './warfare-sky-v18.js?v=1';
 import './warfare-hub-amenities-v19.js?v=1';
+import './warfare-gameplay-audio-v20.js?v=1';
 import './warfare-gameplay-v12.js?v=1';
 import './warfare-v4.js?v=2';
