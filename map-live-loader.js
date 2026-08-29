292e9c38ef3e4750f8996ea2c18a6fddc601345b
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-29 02:03:20 +0100

    Refresh restored title controls chain

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=18";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=19";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
