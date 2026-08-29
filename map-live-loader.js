94bf9cf6fef2493bc80557493dcf6f55903a8f60
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-29 02:10:19 +0100

    Refresh Martin challenge completion loader

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=19";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=20";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
