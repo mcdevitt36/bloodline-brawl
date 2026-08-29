ad2dfc7cffa8dfd564c47c79627105b51137cdd5
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-29 05:08:01 +0100

    Refresh raised title matchup chain

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=21";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=22";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
