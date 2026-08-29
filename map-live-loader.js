eb99f79996c34e7aaa2115dd350c1925ea40b58c
mcdevitt36 <liamclydemcd@gmail.com>
2026-08-29 05:18:59 +0100

    Refresh Nick bat visual chain

--- map-live-loader.js
+++ map-live-loader.js
@@ -138,7 +138,7 @@
                                                             "load",
                                                             () => {
                                                               const titlePositionFix = document.createElement("script");
-                                                              titlePositionFix.src = "title-position-fix-v9.js?v=22";
+                                                              titlePositionFix.src = "title-position-fix-v9.js?v=23";
                                                               document.body.appendChild(titlePositionFix);
                                                             },
                                                             { once: true }
